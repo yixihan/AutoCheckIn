a8598fb810c833d85ec60a8376024701f391b7e1
zengsitong <zengsitong@newhope.cn>
2022-11-21 00:38:39 +0800

    更换cord cloud自动签到登录网址 & 新增nacos

--- src/main/java/com/yixihan/controller/TestController.java
+++ src/main/java/com/yixihan/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.yixihan.controller;
 
 import com.yixihan.auto_check.AutoCheck;
-import com.yixihan.remind.NewHopeRemind;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,21 +18,8 @@ public class TestController {
     @Resource
     private AutoCheck autoCheck;
 
-    @Resource
-    private NewHopeRemind newHopeRemind;
-
     @PostMapping("/runnow")
     public void runNow () {
         autoCheck.autoCheckInCordCloud ();
-    }
-
-    @PostMapping("/remind/newhope/now")
-    public void remindNewHopeNow () {
-        newHopeRemind.remindNow ();
-    }
-
-    @PostMapping("/remind/newhope/reset")
-    public void resetNewHopeNow () {
-        newHopeRemind.resetAtNow ();
     }
 }
