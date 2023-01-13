e9063c3a021cf487a005ab1ba670fb19f950855b
yixihan <200106174970@gmail.com>
2023-01-14 01:27:41 +0800

    fix handshake_failure

--- src/main/java/com/yixihan/AutoCheckinApplication.java
+++ src/main/java/com/yixihan/AutoCheckinApplication.java
@@ -13,6 +13,7 @@ public class AutoCheckinApplication {
 
 
     public static void main(String[] args) {
+        System.setProperty("https.protocols", "TLSv1.2,TLSv1.1,SSLv3");
         SpringApplication.run (AutoCheckinApplication.class, args);
     }
 
